992da25524ca0f81d11fa45d3ec729981d23bcfc
mapleque <mapleque@163.com>
2016-09-27 13:06:55 +0800

    common

--- common/include.php
+++ common/include.php
@@ -2,8 +2,6 @@
 
 ignore_user_abort(true);
 
-require dirname(__FILE__) . '/status.php';
-require dirname(__FILE__) . '/enum.php';
 require dirname(__FILE__) . '/class_loader.php';
 require dirname(__FILE__) . '/base.php';
 require dirname(__FILE__) . '/debug.php';
--- core/base.php
+++ core/base.php
@@ -1,6 +1,8 @@
 <?php
 
 require dirname(__FILE__) . '/../common/include.php';
+require dirname(__FILE__) . '/enum.php';
+require dirname(__FILE__) . '/status.php';
 
 ClassLoader::appendMap([
 	'User'			=> 'user',
