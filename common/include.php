fee1be62bd27559ac150c9ef1fb98578e3f0fde0
mapleque <mapleque@163.com>
2016-09-27 13:25:43 +0800

    rm common

--- common/include.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-ignore_user_abort(true);
-
-require dirname(__FILE__) . '/class_loader.php';
-require dirname(__FILE__) . '/base.php';
-require dirname(__FILE__) . '/debug.php';
-
-ClassLoader::init();
